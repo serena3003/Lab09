dbf6db99dfb6b6312b0467a41adfa7bbafd11339
albertomonge <alberto.monge@polito.it>
2019-05-22 08:35:16 +0200

    Soluzione proposta

--- src/it/polito/tdp/borders/Main.java
+++ src/it/polito/tdp/borders/Main.java
@@ -1,32 +1,34 @@
 package it.polito.tdp.borders;
-
+	
 import it.polito.tdp.borders.model.Model;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("Borders.fxml"));
-			BorderPane root = (BorderPane) loader.load();
-			BordersController controller = loader.getController();
-
-			Model model = new Model();
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Borders.fxml")) ;
+			BorderPane root = (BorderPane)loader.load();
+			BordersController controller = loader.getController() ;
+			
+			Model model = new Model() ;
 			controller.setModel(model);
-
+			
 			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch (Exception e) {
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
